4ba4139b78a19c4decad7e8ae9d8f4c211dfc998
panand1992 <panandiitk@gmail.com>
2022-01-21 18:22:30 +0530

    logs added

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
+const { httpCodes, httpColorCodes } = require("./src/constants/backendConfig");
 
 var indexRouter = require('./routes/index');
 var apiRouter = require('./routes/api');
@@ -14,7 +15,30 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+app.use(logger(function(tokens, req, res) {
+	// get status color
+	var color = httpColorCodes.internalServerError;
+	if (tokens.status(req, res) >= httpCodes.internalServerError) {
+		color = httpColorCodes.internalServerError; //red
+	} else if (tokens.status(req, res) >= httpCodes.badRequest) {
+		color = httpColorCodes.badRequest; //yellow
+	} else if (tokens.status(req, res) >= httpCodes.multipleChoices) {
+		color = httpColorCodes.multipleChoices; //cyan
+	} else if (tokens.status(req, res) >= httpCodes.success) {
+		color = httpColorCodes.success; // green
+	} else {
+		color = 0; // no color
+	}
+	return [tokens.method(req, res),
+		tokens.url(req, res),
+		`\x1b[${color}m${tokens.status(req, res)}`,
+		`\x1b[0m${tokens["response-time"](req, res)}ms - `,
+		tokens.res(req, res, "content-length"),
+		" - ",
+		tokens.date(req, res, "iso"),
+		JSON.stringify(req.headers),
+		JSON.stringify(req.body)].join(" ");
+}));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: false }));
 app.use(cookieParser());
